19eca500f8fefced6c3a36396426b00ed570f1d1
Layne Faler <laynefaler@gmail.com>
2019-05-27 15:07:10 -0700

    Updates to index

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 #!/usr/bin/env node
 
-
-const path = require('path');
 const { exec } = require('shelljs');
 
-const resolutions = [
-    40,
-    60,
-    58,
-    87,
-    80,
-    120,
-    180,
-    20,
-    29,
-    76,
-    156,
-    167,
-    1024,
+const options = [
+    { resolution: 40, prefix: `iphone_notif_x2` },
+    { resolution: 60, prefix: `iphone_notif_x3` },
+    { resolution: 58, prefix: `iphone_settings_x2` },
+    { resolution: 87, prefix: `iphone_settings_x3` },
+    { resolution: 80, prefix: `iphone_spotlight_x2` },
+    { resolution: 120, prefix: `iphone_spotlight_x3` },
+    { resolution: 120, prefix: `iphone_app_x2` },
+    { resolution: 180, prefix: `iphone_app_x3` },
+    { resolution: 20, prefix: `ipad_notif` },
+    { resolution: 40, prefix: `ipad_notif_x2` },
+    { resolution: 29, prefix: `ipad_settings` },
+    { resolution: 58, prefix: `ipad_settings_x2` },
+    { resolution: 40, prefix: `ipad_spotlight` },
+    { resolution: 80, prefix: `ipad_spotlight_x2` },
+    { resolution: 76, prefix: `ipad_app` },
+    { resolution: 152, prefix: `ipad_app_x2` },
+    { resolution: 167, prefix: `ipad_pro_app` },
+    { resolution: 1024, prefix: `app_store` },
 ];
 
 const image_path = process.argv[2];
 const path_trail = image_path.split('/');
 const file_name = path_trail.pop();
 
-resolutions.forEach((num) => {
+options.forEach((data) => {
+    const num = data.resolution;
+    const prefix = data.prefix;
     const size = `${num}x${num}`;
-    const new_file_name = `${size}_${file_name}`;
+    const new_file_name = `${prefix}_${file_name}`;
     const new_file_path = path_trail.slice(0).concat([new_file_name]).join('/');
     
     exec(`magick convert ${image_path} -resize ${size} ${new_file_path}`);
